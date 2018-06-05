aeed27c48638e6d6bf710174f26ffa5004d0d58a
Dan Visel <dbvisel@gmail.com>
2018-06-05 18:48:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A Chrome extension, available online here: https://chrome.google.com/webstore/detail/real-comic-turmp/ckdigppiffmfkmohahfdbekeicgdihff
---
+++ src/content.js
@@ -0,0 +1,4 @@
+var node = document.createElement("style");
+node.innerHTML =
+	'*[data-user-id="25073877"] .tweet-text {font-family: "Comic Sans MS", "Comic Sans";}';
+document.body.appendChild(node);
